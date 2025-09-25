2edbc15a0c72769b00923d9e898f754b7a318cfc
Momoore Oshinaike <momo.mts06@gmail.com>
2025-09-25 17:57:56 -0600

    added hexagon class

---
+++ gitcat/app/src/main/java/com/example/gitcat/Hexagon.java
@@ -0,0 +1,11 @@
+package com.example.gitcat;
+
+public class Hexagon extends Shape{
+
+    
+    private int apothem;
+    public Hexagon(int x, int y, int apothem) {
+        super(x, y);
+        this.apothem = apothem;
+    }
+}
