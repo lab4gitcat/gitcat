221f173852787db3c986757b5ff42d777bee615d
fsoehn <fsoehn@ualberta.ca>
2025-09-25 16:46:08 -0600

    added color to rectangle

--- gitcat/app/src/main/java/com/example/gitcat/Rectangle.java
+++ gitcat/app/src/main/java/com/example/gitcat/Rectangle.java
@@ -4,9 +4,12 @@ public class Rectangle extends Shape {
     private int width;
     private int height;
 
-    public Rectangle (int x, int y, int width, int height){
+    private String color;
+
+    public Rectangle (int x, int y, int width, int height, String color){
         super(x, y);
         this.width = width;
         this.height = height;
+        this.color = color;
     }
 }
