e03533c9cc9448b981ad4d990aa58b52170a5647
Andriy Peleshchyshyn <andriyponline@gmail.com>
2025-09-25 16:59:17 -0600

    added blue color to shape

--- gitcat/app/src/main/java/com/example/gitcat/Shape.java
+++ gitcat/app/src/main/java/com/example/gitcat/Shape.java
@@ -3,8 +3,10 @@ package com.example.gitcat;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color;
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
+        this.color="blue";
     }
 }
