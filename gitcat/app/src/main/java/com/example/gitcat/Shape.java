34fa836ac8198e1444c0a553985617f535ccd91c
ap20051 <125428498+ap20051@users.noreply.github.com>
2025-09-25 17:31:01 -0600

    Update Shape.java color

--- gitcat/app/src/main/java/com/example/gitcat/Shape.java
+++ gitcat/app/src/main/java/com/example/gitcat/Shape.java
@@ -3,6 +3,7 @@ package com.example.gitcat;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "green";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
