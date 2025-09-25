1556cca79fabfbf2067104b0e409ad1c9ff62d3b
Andriy Peleshchyshyn <andriyponline@gmail.com>
2025-09-25 10:06:12 -0600

    added constructor

--- gitcat/app/src/main/java/com/example/gitcat/Shape.java
+++ gitcat/app/src/main/java/com/example/gitcat/Shape.java
@@ -3,4 +3,8 @@ package com.example.gitcat;
 public abstract class Shape {
     private int x;
     private int y;
+    public Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
