ec9aa131c076ccebdc47bba5e3d4d21dff7ce84e
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-09-26 13:38:36 -0600

    cyan cyan

--- gitcat/app/src/main/java/com/example/gitcat/Shape.java
+++ gitcat/app/src/main/java/com/example/gitcat/Shape.java
@@ -3,7 +3,8 @@ package com.example.gitcat;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "blue";
+    private String color = "cyan"; //Resolving merge conflict
+
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
