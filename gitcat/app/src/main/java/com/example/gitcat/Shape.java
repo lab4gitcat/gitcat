ad03f56af212acd61508d76a32f7a1e1170a2797
sandysundae <momo.mts06@gmail.com>
2025-09-25 18:09:30 -0600

    Update Shape.java

--- gitcat/app/src/main/java/com/example/gitcat/Shape.java
+++ gitcat/app/src/main/java/com/example/gitcat/Shape.java
@@ -3,7 +3,7 @@ package com.example.gitcat;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "green";
+    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
