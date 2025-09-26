eaad6c4e69458fb0e211d92a858b3fda13d87c39
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-09-26 13:41:28 -0600

    merge conflict stuff

--- gitcat/app/src/main/java/com/example/gitcat/Shape.java
+++ gitcat/app/src/main/java/com/example/gitcat/Shape.java
@@ -3,7 +3,7 @@ package com.example.gitcat;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    private String color = "blue";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
